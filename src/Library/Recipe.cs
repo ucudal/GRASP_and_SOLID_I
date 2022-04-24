894253b32c900630f498391fc1efb27795466ffe
Juan Riccetto <juanmartinre@gmail.com>
2022-04-24 18:24:52 -0300

    Ejercicio Resuelto

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,14 +25,30 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        /// <summary>
+        /// Nuevamente se cumplen el patrón Expert y el principio SRP, ya que recipe tiene la responsabilidad de llevar a cabo
+        /// los steps, de modo que también debe calcular el precio final.
+        /// </summary>
+        /// <returns></returns>
+        public double GetProductionCost()
+        {
+            double totalCost = 0;
+            foreach (Step step in this.steps)
+            {
+                totalCost += step.StepCost();
+            }
+            return totalCost;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
-            {
+            {   
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+                    $"usando '{step.Equipment.Description}' durante {step.Time}.");
             }
+            Console.WriteLine($"El costo total fue de {this.GetProductionCost()}");
         }
     }
 }
--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -23,5 +23,23 @@ namespace Full_GRASP_And_SOLID.Library
         public int Time { get; set; }
 
         public Equipment Equipment { get; set; }
+
+        /// <summary>
+        /// Se utilizan el patrón Expert y el principio SRP ya que es la propia clase Step la que cumple con la función de 
+        /// saber su costo, lo que hace del código más fácil de extender y más robusto a las modificaciones.
+        /// </summary>
+        /// <returns></returns>
+        public double StepCost()
+        {
+            double supplyCost = 0;
+            double equipmentCost = 0;
+            double totalCost = 0;
+
+            supplyCost = Input.UnitCost * Quantity;
+            equipmentCost = Equipment.HourlyCost * Time;
+            totalCost = supplyCost * equipmentCost;
+
+            return totalCost;
+        }
     }
 }
